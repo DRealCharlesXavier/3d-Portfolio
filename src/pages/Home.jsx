4fee8cc92048b498d6b282800b227e46870059f0
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-10-10 00:16:34 -1100

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
-import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber"
-import Loader from "../components/Loader";
+import React from "react";
+import { Canvas } from "@react-three/fiber";
 
-{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-centre">
   POPUP
-</div> */}
+</div>;
 
 const Home = () => {
   return (
@@ -12,14 +11,9 @@ const Home = () => {
       <Canvas
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}
-      >
-        <Suspense fallback={<Loader />}>
-        
-        </Suspense>
-      </Canvas>
+      ></Canvas>
     </section>
   );
 };
 
 export default Home;
- 
