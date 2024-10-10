be40ec20454fb721e9c7f2c1a5bf1b4eb04e5746
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-10-10 00:37:38 -1100

    2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
+import { Loader } from "../components/Loader";
 
 <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-centre">
   POPUP
@@ -11,7 +12,11 @@ const Home = () => {
       <Canvas
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}
-      ></Canvas>
+      >
+        <Suspense fallback={<Loader />}>
+
+        </Suspense>
+      </Canvas>
     </section>
   );
 };
